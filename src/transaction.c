47ef815a9e293b6b8eea6726662261c1a7c97190
Raphael Müller <raphaelplmuller@icloud.com>
2020-10-27 17:08:53 -0300

    Update transaction definition

--- src/transaction.c
+++ src/transaction.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include "transaction.h"
 
-#define NUMBER_COSTUMERS 5
-#define MAX_TRANSACTION_VALUE 100
+#define COSTUMER_QUANTITY 5
+#define MAX_TRANSACTION_VALUE 1000
 
-Transaction generateTransactionData()
+Transaction *generateTransactionData()
 {
-  int sender_id = generateCustomerId(NUMBER_COSTUMERS);
+  int sender_id = generateCustomerId(COSTUMER_QUANTITY);
   int value = generateValue(MAX_TRANSACTION_VALUE);
   int receiver_id;
 
   do
   {
-    receiver_id = generateCustomerId(NUMBER_COSTUMERS);
-  } while (receiver_id == sender_id); // Prevents receiver_id being equal to sender_id.
+    receiver_id = generateCustomerId(COSTUMER_QUANTITY);
+  } while (!validateDistinctSenderReceiver(sender_id, receiver_id)); // Prevents receiver_id being equal to sender_id.
 
   int type = generateTransactionType();
 
-  Transaction transaction =
-      {
-          .sender_id = sender_id,
-          .receiver_id = receiver_id,
-          .value = value,
-          .type = type};
+  Transaction *transaction = malloc(sizeof(Transaction));
+
+  transaction->sender_id = sender_id;
+  transaction->receiver_id = receiver_id;
+  transaction->value = value;
+  transaction->type = type;
 
   return transaction;
 }
@@ -33,6 +33,18 @@ Transaction generateTransactionDataFromValues(int sender_id, int receiver_id, in
   // TODO
 }
 
+Transaction *generateInvalidTransaction()
+{
+  Transaction *invalid_transaction; 
+
+  invalid_transaction->sender_id = -1;
+  invalid_transaction->receiver_id = -1;
+  invalid_transaction->value = -1;
+  invalid_transaction->type = -1;
+
+  return invalid_transaction;
+}
+
 int generateCustomerId(int max_value)
 {
   return rand() % max_value + 1;
@@ -40,10 +52,35 @@ int generateCustomerId(int max_value)
 
 int generateValue(int max_value)
 {
-  return (rand() % (max_value + 1)) * 10;
+  return (rand() % (max_value / 10 + 1)) * 10;
 }
 
 int generateTransactionType()
 {
   return rand() % 3 + 1;
+}
+
+int validateTransaction(Transaction *transaction)
+{
+  return (validateSender(transaction->sender_id) && validateReceiver(transaction->receiver_id) && validateDistinctSenderReceiver(transaction->sender_id, transaction->receiver_id) && validateType(transaction->type));
+}
+
+static int validateSender(int sender_id)
+{
+  return sender_id > 0 && sender_id <= COSTUMER_QUANTITY;
+}
+
+static int validateReceiver(int receiver_id)
+{
+  return receiver_id > 0 && receiver_id <= COSTUMER_QUANTITY;
+}
+
+static int validateDistinctSenderReceiver(int sender_id, int receiver_id)
+{
+  return sender_id != receiver_id;
+}
+
+static int validateType(int type)
+{
+  return type > 0 && type <= 3;
 }
--- src/transaction.h
+++ src/transaction.h
@@ -9,10 +9,18 @@ typedef struct
   int type;
 } Transaction;
 
-Transaction generateTransactionData();
+Transaction *generateTransactionData();
 Transaction generateTransactionDataFromValues(int sender_id, int receiver_id, int amount, int type);
+Transaction *generateInvalidTransaction();
+
 int generateCustomerId(int max_value);
 int generateValue(int max_value);
 int generateTransactionType();
+int validateTransaction(Transaction *transaction);
+
+static int validateSender(int sender_id);
+static int validateReceiver(int receiver_id);
+static int validateDistinctSenderReceiver(int sender_id, int receiver_id);
+static int validateType(int type);
 
 #endif
